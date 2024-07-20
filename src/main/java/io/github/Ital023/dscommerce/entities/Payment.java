2333862844c32fb393b4268909dd094271c9f4f7
Ital023 <contadoitalo911@gmail.com>
2024-07-19 23:50:10 -0300

    feat: Payment entity

--- src/main/java/io/github/Ital023/dscommerce/entities/Payment.java
+++ src/main/java/io/github/Ital023/dscommerce/entities/Payment.java
@@ -11,8 +11,8 @@ import java.time.Instant;
 @AllArgsConstructor
 @Data
 @Entity
-@Table(name = "tb_order")
-public class Order {
+@Table(name = "tb_payment")
+public class Payment {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,13 +21,8 @@ public class Order {
     @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant moment;
 
-    private OrderStatus orderStatus;
-
-    @ManyToOne
-    @JoinColumn(name = "client_id")
-    private User client;
-
-    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
-    private Payment payment;
+    @OneToOne
+    @MapsId
+    private Order order;
 
 }
