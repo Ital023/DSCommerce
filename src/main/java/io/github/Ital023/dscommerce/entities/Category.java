989c99fc4f96ee6b9ce194505fa5e8283fc9f1ae
Ital023 <contadoitalo911@gmail.com>
2024-07-20 00:07:06 -0300

    feat: Product and Category entity

---
+++ src/main/java/io/github/Ital023/dscommerce/entities/Category.java
@@ -0,0 +1,27 @@
+package io.github.Ital023.dscommerce.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "tb_category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Product> products = new HashSet<>();
+
+}
