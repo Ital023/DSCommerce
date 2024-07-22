727af0d94555fe85da3251673a6fd3560a7a9d0c
Ital023 <contadoitalo911@gmail.com>
2024-07-22 00:11:30 -0300

    feat: Creating validation ProductDTO

--- src/main/java/io/github/Ital023/dscommerce/controllers/ProductController.java
+++ src/main/java/io/github/Ital023/dscommerce/controllers/ProductController.java
@@ -3,6 +3,7 @@ package io.github.Ital023.dscommerce.controllers;
 import io.github.Ital023.dscommerce.dto.ProductDTO;
 import io.github.Ital023.dscommerce.entities.Product;
 import io.github.Ital023.dscommerce.services.ProductService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -34,7 +35,7 @@ public class ProductController {
     }
 
     @PostMapping
-    public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO productDTO) {
+    public ResponseEntity<ProductDTO> insert(@Valid @RequestBody ProductDTO productDTO) {
         productDTO = productService.insert(productDTO);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
                 .buildAndExpand(productDTO.getId())
@@ -43,7 +44,7 @@ public class ProductController {
     }
 
     @PutMapping(value = "/{id}")
-    public ResponseEntity<ProductDTO> findById(@PathVariable Long id, @RequestBody ProductDTO productDTO) {
+    public ResponseEntity<ProductDTO> update(@PathVariable Long id, @Valid @RequestBody ProductDTO productDTO) {
          productDTO = productService.update(productDTO, id);
         return ResponseEntity.ok(productDTO);
     }
